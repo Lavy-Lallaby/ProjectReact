3bed088eb8e6f097a21e5820f04ef48640a549be
Lavy-Lallaby <77435549+Lavy-Lallaby@users.noreply.github.com>
2022-01-19 14:31:49 +0700

    summary

---
+++ App.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen';
+import AboutScreen from './screens/AboutScreen';
+
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+     <Stack.Navigator initailRouteName='Home'
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#C45',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}
+     >
+        <Stack.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{title:'Home'}}
+        />
+        <Stack.Screen
+        name="AboutScreen"
+        component={AboutScreen}
+        options={{title:'About'}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App
